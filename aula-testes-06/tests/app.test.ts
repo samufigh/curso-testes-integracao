e654e33821cd2d0b8602a8589dce1af9bc81b813
samufigh <samufigh21@gmail.com>
2023-09-28 00:44:19 -0300

    exercício-06

---
+++ aula-testes-06/tests/app.test.ts
@@ -0,0 +1,19 @@
+import supertest from "supertest";
+
+import app from "./../src/app";
+
+const api = supertest(app);
+
+describe("API test", () => {
+
+  it("validação de /event", async() => {
+    const result = await api.get("/event");
+  expect(result.status).toBe(200)
+  expect(result.body).toEqual({
+    id: expect.any(Number),
+    title: "Super Event!",
+    image: "https://img.freepik.com/fotos-gratis/publico-animado-assistindo-fogos-de-artificio-de-confete-e-se-divertindo-no-festival-de-musica-a-noite-copiar-espaco_637285-559.jpg",
+    date: "2023-07-21T00:00:00.000Z"
+  })
+  })
+});
